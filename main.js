f8c36985d4645106720f73cfd31dd6b16b01d456
Bharati Subramanian <bharatisharada@gmail.com>
2021-08-29 22:07:10 +0530

    feat: play sounds

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-document.querySelector('.btn-a').addEventListener('click', () => {
-    // alert('play');
-    document.querySelector('.sound').play();
-});
+const soundButtons = document.querySelectorAll('.btn');
+
+document.addEventListener('DOMContentLoaded', () => {
+    soundButtons.forEach(btn => {
+        btn.addEventListener('click', playSound);
+        console.log(btn);
+    });
+})
+
+
+function playSound(e) {
+    let target  = e.target;
+
+    if(target.tagName === 'SPAN') {
+        target = target.parentElement;
+    }
+
+    const audio = target.firstElementChild;
+    audio.play();
+
+    target.classList.add('play');
+    
+    setTimeout(() => {
+        target.classList.remove('play');
+    }, 200);
+}
+
