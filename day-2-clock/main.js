dbf01172100be0d482291eaf7597431fd5f9495f
Bharati Subramanian <bharatisharada@gmail.com>
2021-08-31 13:07:30 +0530

    edit styles: hand angles and transition

--- day-2-clock/main.js
+++ day-2-clock/main.js
@@ -3,41 +3,66 @@
     const secondHand = document.querySelector('.second-hand');
     const minuteHand = document.querySelector('.minute-hand');
     
+    const clockHand = document.querySelector('.clock-hand');
     function rotateHands() {
-        const hour = new Date().getHours();
-        const minute = new Date().getMinutes();
-        const second = new Date().getSeconds();
+    const hour = new Date().getHours();
+    const minute = new Date().getMinutes();
+    const second = new Date().getSeconds();
 
-        if(second == 0) {
-            secondHand.style.transition = "none";
-            // secondHand.style.transform = "none"
-        }
-        else {
-            secondHand.style.transition = "transform 0.5s cubic-bezier(0.41, 0.11, 0.46, 1.28)";
-        }
+    const secondAngle = (second * 6);
+    const minuteAngle = (minute * 6) ;
+    const hourAngle = (hour * 30 + minute/2);
 
         
-
-
-        const hourAngle = ((hour/ 12) * 360) + 90;
-        const minuteAngle = ((minute / 60) * 360) + 90;
-        const secondAngle = ((second / 60) * 360) + 90;
-        
-        secondHand.style.transform = `rotate(${secondAngle}deg)`;
-        hourHand.style.transform = `rotate(${hourAngle}deg)`
-        minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
     
+    
+    // console.log(secondAngle, second);
+    if(second === 0) {
+        secondHand.style.transitionDuration = `0s`;
     }
+    else {
+        secondHand.style.transitionDuration = `0.09s`;
+    }
+    if(hour === 0 || hour == 12) {
+        hourHand.style.transitionDuration = `0s`;
+    }
+    else {
+        hourHand.style.transitionDuration = `0.09s`;
+    }
+    if(minute === 0) {
+        minuteHand.style.transitionDuration = `0s`;
+    }
+    else {
+        minuteHand.style.transitionDuration = `0.09s`;
+    }
+    
+    
+    
+          
+    secondHand.style.transform = `rotate(${secondAngle}deg)`;
+    hourHand.style.transform = `rotate(${hourAngle}deg)`
+    minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+    
+}
    
-    // document.querySelector('.hour-hand').style.transform = `rotate(75deg)`;
+setInterval(rotateHands, 1000);
 
-    setInterval(rotateHands, 1000);
+
 
     /*
-        12 -> 90deg
-        11 -> 80deg
-        6 -> -90deg
-        3 -> 180deg
-        9 -> 0 deg
 
-    */
+12 hours = 360 deg
+1 hour = 30deg
+h hours = 30h deg;
+60mins = 30deg;
+1mins = 1/2 deg;
+m mins = m/2 deg
+
+MINUTE
+60 mins = 360deg
+1 mins = 6deg
+
+60sec = 360deg;
+1 sec = 6deg;
+
+*/
