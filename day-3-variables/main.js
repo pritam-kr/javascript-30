975373ba3944d3a93760ec17d269c2f7e393d5f6
Bharati Subramanian <bharatisharada@gmail.com>
2021-09-01 10:59:04 +0530

    edit: set css variable values using js

--- day-3-variables/main.js
+++ day-3-variables/main.js
@@ -5,7 +5,6 @@ const paddingVar = rootStyles.getPropertyValue('--image-padding');
 const bgColorVar = rootStyles.getPropertyValue('--image-background-color');
 const blurVar = rootStyles.getPropertyValue('--image-blur');
 
-
 const paddingRangeInput = document.querySelector('#range-padding');
 const blurRangeInput = document.querySelector('#range-blur');
 const bgColorInput = document.querySelector('#bg-color-picker');
@@ -15,16 +14,27 @@ let padding = paddingRangeInput.value;
 let blurVal = blurRangeInput.value;
 let bgColor = bgColorInput.value;
 
+const spanPadding = document.querySelector('.value-padding');
+const spanBlur = document.querySelector('.value-blur');
+const spanBgColor = document.querySelector('.value-bg-color')
+
+spanPadding.textContent = padding + paddingRangeInput.getAttribute('data-sizing');
+
+spanBlur.textContent = blurVal + blurRangeInput.getAttribute('data-sizing');
+
+spanBgColor.textContent = bgColor;
+
+
 // console.log(paddingRangeInput, blurRangeInput, bgColorInput);
 
 
 
-blurRangeInput.addEventListener('change', () => {
+blurRangeInput.addEventListener('input', () => {
     blurVal = blurRangeInput.value;
     setStyle();
 });
 
-paddingRangeInput.addEventListener('change', () => {
+paddingRangeInput.addEventListener('input', () => {
     padding = paddingRangeInput.value;
     setStyle();
 });
@@ -36,12 +46,21 @@ bgColorInput.addEventListener('input', () => {
 
 function setStyle() {
     let unit = blurRangeInput.getAttribute('data-sizing');
-    image.style.filter = `blur(${blurVal + unit})`;
+    // image.style.filter = `blur(${blurVal + unit})`;
+    root.style.setProperty('--image-blur', (blurVal + unit));
 
     unit = paddingRangeInput.getAttribute('data-sizing');
-    image.style.padding = `${padding + unit}`;
-    
-    image.style.backgroundColor = `${bgColor}`;
+    // image.style.padding = `${padding + unit}`;
+    root.style.setProperty('--image-padding', (padding + unit));
+
+    root.style.setProperty('--image-background-color', bgColor);
+
+    spanPadding.textContent = rootStyles.getPropertyValue('--image-padding');
+
+    spanBlur.textContent = rootStyles.getPropertyValue('--image-blur');
+
+
+    spanBgColor.textContent = rootStyles.getPropertyValue('--image-background-color');
 
 }
 
